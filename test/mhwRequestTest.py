b220846a9f7134d2b2506acdfa4e5d496a56468b
Sturdevant, Kyle <kyle.sturdevant@gmail.com>
2021-04-19 22:33:56 -0400

    Refactored duplicate code, Add async functionality, add error handling, and tests for errors.

--- test/mhwRequestTest.py
+++ test/mhwRequestTest.py
@@ -2,8 +2,13 @@ import mhwRequest
 
 
 def test_get_all():
-    results = mhwRequest.get_all("monsters")
+    results = mhwRequest.get_all("locations")
     assert(len(results) > 0)
+
+
+def test_get_all_wrong_type():
+    results = mhwRequest.get_all("asdf")
+    assert("error" in results.keys())
 
 
 def test_get_id():
@@ -12,7 +17,22 @@ def test_get_id():
     assert(results["name"] == "Jagras")
 
 
+def test_get_id_wrong_type():
+    results = mhwRequest.get_id("asdf", "2")
+    assert("error" in results.keys())
+
+
+def test_get_id_wrong_id():
+    results = mhwRequest.get_id("monsters", "100")
+    assert("error" in results.keys())
+
+
 def test_get_name():
     results = mhwRequest.get_name("monsters", "gaj")
     assert(len(results) > 0)
     assert(results[0]["name"] == "Gajau")
+
+
+def test_get_name_no_match():
+    results = mhwRequest.get_name("monsters", "zzz")
+    assert(len(results) == 0)
