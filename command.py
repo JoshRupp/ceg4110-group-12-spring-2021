660601aab65143c58b194df6c76a8dc77b150a80
Kazalor <71352222+Kazalor@users.noreply.github.com>
2021-04-21 23:13:59 -0400

    Added to command.py to host the functions called by the commands in main.py

--- command.py
+++ command.py
@@ -1,47 +1,47 @@
 def overview():
   return "The overview command was called"
 
-def monster(name):
-  if name == None:
-    return "Called monster with no input"
-  else:
-    return "Called monster with the input: " + str(name)
+def monster(input):
+  return "Called monster with the input: " + str(input)
+
+def helpMonster():
+  return "Monster was called with no args"
 
 def weapon(name):
-  if name == None:
-    return "Called weapon with no input"
-  else:
-    return "Called weapon with the input: " + str(name)
+  return "Called weapon with the input: " + str(name)
+
+def helpWeapon():
+  return "Monster was called with no args"
 
 def material(name):
-  if name == None:
-    return "Called material with no input"
-  else:
-    return "Called material with the input: " + str(name)
+  return "Called material with the input: " + str(name)
+
+def helpMaterial():
+  return "Material was called with no args"
 
 def save(weapon, armor, decoration):
-  if weapon == None and armor == None and decoration == None:
-    return "Called save with no input"
-  else:
-    return "Called save with the inputs: " + str(weapon) + str(armor) + str(decoration)
+  return "Called save with the inputs: " + str(weapon) + str(armor) + str(decoration)
+
+def helpSave():
+  return "Save was called with no args"
 
 def vendor():
   return "Vendor was called"
 
 def products(vendor):
-  if vendor == None:
-    return "Called products with no input"
-  else:
-    return "Called products with the input: " + str(vendor)
+  return "Called products with the input: " + str(vendor)
+
+def helpProducts():
+  return "Products was called with no args"
 
 def events():
   return "Events were called"
 
 def location(ecosystem, rank):
-  if vendor == None and rank == None:
-    return "Called location with no input"
-  else:
-    return "Called location with the input: " + str(ecosystem) + str(rank)
+  return "Called location with the input: " + str(ecosystem) + str(rank)
+
+def helpLocation():
+  return "Location was called with no args"
 
 def huntersTools():
   return "Called hunters tools"
@@ -50,37 +50,37 @@ def tip():
   return "Called tip"
 
 def guiding(area, level):
-  if area == None and level == None:
-    return "Called guiding with no input"
-  else:
-    return "Called guiding with the input: " + str(area) + str(level)
+  return "Called guiding with the input: " + str(area) + str(level)
+
+def helpGuiding():
+  return "Guiding was called with no args"
 
 def item(name, rarity, type):
-  if name == None and rarity == None and type == None:
-    return "Called item with no input"
-  else:
-    return "Called item with the inputs: " + str(name) + str(rarity) + str(type)
+  return "Called item with the inputs: " + str(name) + str(rarity) + str(type)
+
+def helpItem():
+  return "Item was called with no args"
 
 def defeatedMonster(name):
-  if name == None:
-    return "Called defeated monster with no input"
-  else:
-    return "Called defeated monster with the input: " + str(name)
+  return "Called defeated monster with the input: " + str(name)
+
+def helpDefeatedMonster():
+  return "Defeated monster was called with no args"
 
 def whoKilledMe(name):
-  if name == None:
-    return "Called who killed me with no input"
-  else:
-    return "Called who killed me with the input: " + str(name)
+  return "Called who killed me with the input: " + str(name)
+
+def helpWhoKilledMe():
+  return "Who killed me was called with no args"
 
 def armor(name):
-  if name == None:
-    return "Called armor with no input"
-  else:
-    return "Called armor with the input: " + str(name)
+  return "Called armor with the input: " + str(name)
+
+def helpArmor():
+  return "Armor was called with no args"
 
 def skill(name):
-  if name == None:
-    return "Called skill with no input"
-  else:
-    return "Called skill with the input: " + str(name)
+  return "Called skill with the input: " + str(name)
+
+def helpSkill():
+  return "Skill was called with no args"
