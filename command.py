c1d76f1b4237c8641e3c13dc78053a37f559542f
Kazalor <71352222+Kazalor@users.noreply.github.com>
2021-04-22 18:18:57 -0400

    Changed command.py to fit python conventions

--- command.py
+++ command.py
@@ -1,86 +1,114 @@
 def overview():
-  return "The overview command was called"
+    return "The overview command was called"
 
-def monster(input):
-  return "Called monster with the input: " + str(input)
 
-def helpMonster():
-  return "Monster was called with no args"
+def monster(args):
+    return "Called monster with the input: " + str(args)
 
-def weapon(name):
-  return "Called weapon with the input: " + str(name)
 
-def helpWeapon():
-  return "Monster was called with no args"
+def help_monster():
+    return "Monster was called with no args"
 
-def material(name):
-  return "Called material with the input: " + str(name)
 
-def helpMaterial():
-  return "Material was called with no args"
+def weapon(args):
+    return "Called weapon with the input: " + str(args)
 
-def save(weapon, armor, decoration):
-  return "Called save with the inputs: " + str(weapon) + str(armor) + str(decoration)
 
-def helpSave():
-  return "Save was called with no args"
+def help_weapon():
+    return "Monster was called with no args"
+
+
+def material(args):
+    return "Called material with the args: " + str(args)
+
+
+def help_material():
+    return "Material was called with no args"
+
+
+def save(args):
+    return "Called save with the inputs: " + str(args)
+
+
+def help_save():
+    return "Save was called with no args"
+
 
 def vendor():
-  return "Vendor was called"
+    return "Vendor was called"
 
-def products(vendor):
-  return "Called products with the input: " + str(vendor)
 
-def helpProducts():
-  return "Products was called with no args"
+def products(args):
+    return "Called products with the input: " + str(args)
+
+
+def help_products():
+    return "Products was called with no args"
+
 
 def events():
-  return "Events were called"
+    return "Events were called"
 
-def location(ecosystem, rank):
-  return "Called location with the input: " + str(ecosystem) + str(rank)
 
-def helpLocation():
-  return "Location was called with no args"
+def location(args):
+    return "Called location with the input: " + str(args)
 
-def huntersTools():
-  return "Called hunters tools"
+
+def help_location():
+    return "Location was called with no args"
+
+
+def hunters_tools():
+    return "Called hunters tools"
+
 
 def tip():
-  return "Called tip"
+    return "Called tip"
 
-def guiding(area, level):
-  return "Called guiding with the input: " + str(area) + str(level)
 
-def helpGuiding():
-  return "Guiding was called with no args"
+def guiding(args):
+    return "Called guiding with the input: " + str(args)
 
-def item(name, rarity, type):
-  return "Called item with the inputs: " + str(name) + str(rarity) + str(type)
 
-def helpItem():
-  return "Item was called with no args"
+def help_guiding():
+    return "Guiding was called with no args"
 
-def defeatedMonster(name):
-  return "Called defeated monster with the input: " + str(name)
 
-def helpDefeatedMonster():
-  return "Defeated monster was called with no args"
+def item(args):
+    return "Called item with the inputs: " + str(args)
 
-def whoKilledMe(name):
-  return "Called who killed me with the input: " + str(name)
 
-def helpWhoKilledMe():
-  return "Who killed me was called with no args"
+def help_item():
+    return "Item was called with no args"
 
-def armor(name):
-  return "Called armor with the input: " + str(name)
 
-def helpArmor():
-  return "Armor was called with no args"
+def defeated_monster(args):
+    return "Called defeated monster with the input: " + str(args)
 
-def skill(name):
-  return "Called skill with the input: " + str(name)
 
-def helpSkill():
-  return "Skill was called with no args"
+def help_defeated_monster():
+    return "Defeated monster was called with no args"
+
+
+def who_killed_me(args):
+    return "Called who killed me with the input: " + str(args)
+
+
+def help_who_killed_me():
+    return "Who killed me was called with no args"
+
+
+def armor(args):
+    return "Called armor with the input: " + str(args)
+
+
+def help_armor():
+    return "Armor was called with no args"
+
+
+def skill(args):
+    return "Called skill with the input: " + str(args)
+
+
+def help_skill():
+    return "Skill was called with no args"
